d8f58c759f1ecbe3623093c3c31bf59797e35b28
Xfurry <mail@nomail.com>
2011-06-29 01:31:19 +0300

    010 - boss script for Eregos and Urom

--- scripts/northrend/nexus/oculus/boss_eregos.cpp
+++ scripts/northrend/nexus/oculus/boss_eregos.cpp
@@ -35,6 +35,22 @@ enum
     SAY_KILL_2                      = -1578018,
     SAY_KILL_3                      = -1578019,
     SAY_DEATH                       = -1578020,
+
+    // spells
+    SPELL_ARCANE_BARRAGE            = 50804,
+    SPELL_ARCANE_BARRAGE_H          = 59381,
+    SPELL_ARCANE_VOLLEY             = 51153,
+    SPELL_ARCANE_VOLLEY_H           = 59382,
+    SPELL_ENRAGED_ASSAULT           = 51170,
+    SPELL_PLANAR_ANOMALIES          = 57959,
+    SPELL_SUMMON_LEY_WHELP          = 51175,
+    SPELL_PLANAR_SHIFT              = 51162,
+
+    MAX_WHELPS                      = 2,        // The max number of whelps summoned at once
+    MAX_ANOMALIES                   = 5,
+
+    //NPC_PLANAR_ANOMALY              = 30879,
+    //NPC_GREATER_LEY_WHELP           = 28276,
 };
 
 struct MANGOS_DLL_DECL boss_eregosAI : public ScriptedAI
@@ -49,8 +65,19 @@ struct MANGOS_DLL_DECL boss_eregosAI : public ScriptedAI
     ScriptedInstance* m_pInstance;
     bool m_bIsRegularMode;
 
+    uint32 m_uiArcaneBarrageTimer;
+    uint32 m_uiArcaneVolleyTimer;
+    uint32 m_uiEnrageTimer;
+    uint32 m_uiSummonWhelpsTimer;
+    uint8 m_uiHpPercent;
+
     void Reset()
     {
+        m_uiArcaneBarrageTimer  = 5000;
+        m_uiArcaneVolleyTimer   = 15000;
+        m_uiEnrageTimer         = 25000;
+        m_uiSummonWhelpsTimer   = 2000;
+        m_uiHpPercent           = 60;
     }
 
     void Aggro(Unit* pWho)
@@ -63,6 +90,12 @@ struct MANGOS_DLL_DECL boss_eregosAI : public ScriptedAI
 
     void KilledUnit(Unit* pVictim)
     {
+        switch(urand(0, 2))
+        {
+            case 0: DoScriptText(SAY_KILL_1, m_creature); break;
+            case 1: DoScriptText(SAY_KILL_2, m_creature); break;
+            case 2: DoScriptText(SAY_KILL_3, m_creature); break;
+        }
     }
 
     void JustDied(Unit* pKiller)
@@ -79,10 +112,77 @@ struct MANGOS_DLL_DECL boss_eregosAI : public ScriptedAI
             m_pInstance->SetData(TYPE_EREGOS, FAIL);
     }
 
+    void JustSummoned(Creature* pSummoned)
+    {
+        if (Unit* pTarget = m_creature->SelectAttackingTarget(ATTACKING_TARGET_RANDOM, 0))
+            pSummoned->AI()->AttackStart(pTarget);
+    }
+
     void UpdateAI(const uint32 uiDiff)
     {
-        if (!m_creature->SelectHostileTarget() || !m_creature->getVictim())
+        if (!m_creature->SelectHostileTarget() || !m_creature->getVictim() || m_creature->HasAura(SPELL_PLANAR_SHIFT))
             return;
+
+        if(m_creature->GetHealthPercent() < m_uiHpPercent)
+        {
+            if (DoCastSpellIfCan(m_creature, SPELL_PLANAR_SHIFT) == CAST_OK)
+            {
+                // summon 5 anomalies (shold it depend on the players in instance?)
+                for(uint8 i = 0; i < MAX_ANOMALIES; i++)
+                    DoCastSpellIfCan(m_creature, SPELL_PLANAR_ANOMALIES, CAST_TRIGGERED);
+
+                switch(urand(0, 2))
+                {
+                    case 0: DoScriptText(SAY_ARCANE_SHIELD, m_creature); break;
+                    case 1: DoScriptText(SAY_FIRE_SHIELD, m_creature); break;
+                    case 2: DoScriptText(SAY_NATURE_SHIELD, m_creature); break;
+                }
+
+                // set next phase to 20%
+                m_uiHpPercent -= 40;
+            }
+        }
+
+        if(m_uiArcaneBarrageTimer < uiDiff)
+        {
+            if (Unit* pTarget = m_creature->SelectAttackingTarget(ATTACKING_TARGET_RANDOM, 0))
+            {
+                if (DoCastSpellIfCan(pTarget, m_bIsRegularMode ? SPELL_ARCANE_BARRAGE : SPELL_ARCANE_BARRAGE_H) == CAST_OK)
+                    m_uiArcaneBarrageTimer = urand(3000, 5000);
+            }
+        }
+        else
+            m_uiArcaneBarrageTimer -= uiDiff;
+
+        if(m_uiSummonWhelpsTimer < uiDiff)
+        {
+            for(uint8 i = 0; i < MAX_WHELPS; i++)
+            {
+                if (DoCastSpellIfCan(m_creature, SPELL_SUMMON_LEY_WHELP, CAST_TRIGGERED) == CAST_OK)
+                    m_uiSummonWhelpsTimer = 20000;
+            }
+        }
+        else
+            m_uiSummonWhelpsTimer -= uiDiff;
+
+        if(m_uiArcaneVolleyTimer < uiDiff)
+        {
+            if (DoCastSpellIfCan(m_creature, m_bIsRegularMode ? SPELL_ARCANE_VOLLEY : SPELL_ARCANE_VOLLEY_H) == CAST_OK)
+                m_uiArcaneVolleyTimer = urand(12000, 15000);
+        }
+        else
+            m_uiArcaneVolleyTimer -= uiDiff;
+
+        if(m_uiEnrageTimer < uiDiff)
+        {
+            if (DoCastSpellIfCan(m_creature, SPELL_ENRAGED_ASSAULT) == CAST_OK)
+            {
+                DoScriptText(SAY_FRENZY, m_creature);
+                m_uiEnrageTimer = urand(20000, 30000);
+            }
+        }
+        else
+            m_uiEnrageTimer -= uiDiff;
 
         DoMeleeAttackIfReady();
     }
--- scripts/northrend/nexus/oculus/boss_urom.cpp
+++ scripts/northrend/nexus/oculus/boss_urom.cpp
@@ -36,6 +36,44 @@ enum
     SAY_KILL_2                      = -1578008,
     SAY_KILL_3                      = -1578009,
     SAY_DEATH                       = -1578010,
+
+    // spells
+    SPELL_ARCANE_SHIELD             = 53813,
+    SPELL_ARCANE_EXPLOSION          = 51110,
+    SPELL_ARCANE_EXPLOSION_H        = 59377,
+    SPELL_FROSTBOMB                 = 51103,
+    SPELL_TIME_BOMB                 = 51121,
+    SPELL_TIME_BOMB_H               = 59376,
+    SPELL_SUMMON_MENAGERIE_1        = 50476,
+    SPELL_SUMMON_MENAGERIE_2        = 50495,
+    SPELL_SUMMON_MENAGERIE_3        = 50496,
+    SPELL_TELEPORT                  = 51112,
+
+    // npcs
+    NPC_PHANTASMAL_CLOUDSCRAPER     = 27645,
+    NPC_PHANTASMAL_MAMMOTH          = 27642,
+    NPC_PHANTASMAL_WOLF             = 27644,
+
+    NPC_PHANTASMAL_AIR              = 27650,
+    NPC_PHANTASMAL_FIRE             = 27651,
+    NPC_PHANTASMAL_WATER            = 27653,
+
+    NPC_PHANTASMAL_MURLOC           = 27649,
+    NPC_PHANTASMAL_NAGAL            = 27648,
+    NPC_PHANTASMAL_OGRE             = 27647,
+
+    MAX_MOBS                        = 3,
+
+    // phases
+    PHASE_PLATFORMS                 = 0,
+    PHASE_CENTER                    = 1
+};
+
+static uint32 uiTrashPacks[MAX_MOBS][MAX_MOBS]=
+{
+    {NPC_PHANTASMAL_CLOUDSCRAPER,   NPC_PHANTASMAL_MAMMOTH, NPC_PHANTASMAL_WOLF},
+    {NPC_PHANTASMAL_AIR,            NPC_PHANTASMAL_FIRE,    NPC_PHANTASMAL_WATER},
+    {NPC_PHANTASMAL_MURLOC,         NPC_PHANTASMAL_NAGAL,   NPC_PHANTASMAL_OGRE},
 };
 
 struct MANGOS_DLL_DECL boss_uromAI : public ScriptedAI
@@ -44,26 +82,56 @@ struct MANGOS_DLL_DECL boss_uromAI : public ScriptedAI
     {
         m_pInstance = (ScriptedInstance*)pCreature->GetInstanceData();
         m_bIsRegularMode = pCreature->GetMap()->IsRegularDifficulty();
+
+        // randomize mobs
+        for(uint8 i = 0; i < MAX_MOBS; ++i)
+            m_vuiTrashPacksIds.push_back(i);
+
+        std::random_shuffle(m_vuiTrashPacksIds.begin(), m_vuiTrashPacksIds.end());
+
         Reset();
     }
 
     ScriptedInstance* m_pInstance;
     bool m_bIsRegularMode;
 
+    uint8 m_uiPhase;
+    uint8 m_uiPlatformPhase;
+    bool m_bIsTeleporting;
+    uint32 m_uiExplosionTimer;
+    uint32 m_uiTeleportTimer;
+    uint32 m_uiFrostBombTimer;
+    uint32 m_uiTimeBombTimer;
+
+    std::vector<uint32> m_vuiTrashPacksIds;
+
     void Reset()
     {
+        DoCastSpellIfCan(m_creature, SPELL_ARCANE_SHIELD);
+
+        m_bIsTeleporting    = false;
+        m_uiPlatformPhase   = 0;
+        m_uiExplosionTimer  = 0;
+        m_uiPhase           = PHASE_PLATFORMS;
+        m_uiTeleportTimer   = urand(30000, 35000);
+        m_uiFrostBombTimer  = urand(5000, 8000);
+        m_uiTimeBombTimer   = urand(20000, 25000);
     }
 
     void Aggro(Unit* pWho)
     {
-        DoScriptText(SAY_AGGRO, m_creature);
-
         if (m_pInstance)
             m_pInstance->SetData(TYPE_UROM, IN_PROGRESS);
     }
 
     void KilledUnit(Unit* pVictim)
     {
+        switch(urand(0, 2))
+        {
+            case 0: DoScriptText(SAY_KILL_1, m_creature); break;
+            case 1: DoScriptText(SAY_KILL_2, m_creature); break;
+            case 2: DoScriptText(SAY_KILL_3, m_creature); break;
+        }
     }
 
     void JustDied(Unit* pKiller)
@@ -80,12 +148,151 @@ struct MANGOS_DLL_DECL boss_uromAI : public ScriptedAI
             m_pInstance->SetData(TYPE_UROM, FAIL);
     }
 
+    void AttackStart(Unit* pWho)
+    {
+        if (m_uiPlatformPhase < 3)
+        {
+            if (!m_bIsTeleporting)
+            {
+                // Don't take damage, just summon the trash mobs pack
+                m_bIsTeleporting = true;
+                m_creature->InterruptNonMeleeSpells(false);
+                DoSpawnTrashPack();
+
+                // teleport to next platform and spawn adds
+                switch(m_uiPlatformPhase)
+                {
+                    case 0:
+                        if (DoCastSpellIfCan(m_creature, SPELL_SUMMON_MENAGERIE_1) == CAST_OK)
+                            DoScriptText(SAY_SUMMON_1, m_creature);
+                        break;
+                    case 1:
+                        if (DoCastSpellIfCan(m_creature, SPELL_SUMMON_MENAGERIE_2) == CAST_OK)
+                            DoScriptText(SAY_SUMMON_2, m_creature);
+                        break;
+                    case 2:
+                        if (DoCastSpellIfCan(m_creature, SPELL_SUMMON_MENAGERIE_3) == CAST_OK)
+                            DoScriptText(SAY_SUMMON_3, m_creature);
+                        break;
+                }
+            }
+
+            return;
+        }
+        else
+        {
+            if (m_uiPhase == PHASE_PLATFORMS)
+            {
+                DoScriptText(SAY_AGGRO, m_creature);
+                m_uiPhase = PHASE_CENTER;
+                m_creature->InterruptNonMeleeSpells(false);
+            }
+            ScriptedAI::AttackStart(pWho);
+        }
+    }
+
+    void JustSummoned(Creature* pSummon)
+    {
+        if (Unit* pTarget = m_creature->SelectAttackingTarget(ATTACKING_TARGET_RANDOM, 0))
+            pSummon->AI()->AttackStart(pTarget);
+    }
+
+    void DoSpawnTrashPack()
+    {
+        float fX, fY, fZ;
+        for(uint8 i = 0; i < MAX_MOBS; ++i)
+        {
+            m_creature->GetNearPoint(m_creature, fX, fY, fZ, 0, 10.0f, 2*M_PI_F/3 * i);
+            m_creature->SummonCreature(uiTrashPacks[m_vuiTrashPacksIds[m_uiPlatformPhase]][i], fX, fY, fZ, 0.0f, TEMPSUMMON_TIMED_DESPAWN_OUT_OF_COMBAT, 30000);
+        }
+    }
+
+    void DamageTaken(Unit* pDoneBy, uint32 &uiDamage)
+    {
+        if (m_creature->HasAura(SPELL_ARCANE_SHIELD) || m_uiPhase == PHASE_PLATFORMS)
+            uiDamage = 0;
+    }
+
+    void SpellHit(Unit* pCaster, const SpellEntry* pSpell)
+    {
+        switch(pSpell->Id)
+        {
+            case SPELL_SUMMON_MENAGERIE_3:
+            case SPELL_SUMMON_MENAGERIE_2:
+            case SPELL_SUMMON_MENAGERIE_1:
+                m_creature->RemoveAllAuras();
+                DoCastSpellIfCan(m_creature, SPELL_ARCANE_SHIELD);
+                m_bIsTeleporting = false;
+                ++m_uiPlatformPhase;
+                break;
+            case SPELL_TELEPORT:
+                DoCastSpellIfCan(m_creature, m_bIsRegularMode ? SPELL_ARCANE_EXPLOSION : SPELL_ARCANE_EXPLOSION_H, CAST_TRIGGERED);
+                break;
+        }
+    }
+
     void UpdateAI(const uint32 uiDiff)
     {
-        if (!m_creature->SelectHostileTarget() || !m_creature->getVictim())
-            return;
+        if (m_uiPhase == PHASE_PLATFORMS)
+        {
+            // set the boss shield
+            if (!m_creature->HasAura(SPELL_ARCANE_SHIELD))
+                DoCastSpellIfCan(m_creature, SPELL_ARCANE_SHIELD);
+        }
+        else if (m_uiPhase == PHASE_CENTER)
+        {
+            if (!m_creature->SelectHostileTarget() || !m_creature->getVictim())
+                return;
 
-        DoMeleeAttackIfReady();
+            if (m_uiExplosionTimer)
+            {
+                if (m_uiExplosionTimer <= uiDiff)
+                {
+                    if (DoCastSpellIfCan(m_creature, m_bIsRegularMode ? SPELL_ARCANE_EXPLOSION : SPELL_ARCANE_EXPLOSION_H) == CAST_OK)
+                    {
+                        m_creature->GetMotionMaster()->Clear();
+                        m_creature->GetMotionMaster()->MoveChase(m_creature->getVictim());
+                        m_uiExplosionTimer = 0;
+                    }
+                }
+                else
+                    m_uiExplosionTimer -= uiDiff;
+            }
+
+            if (m_uiTeleportTimer < uiDiff)
+            {
+                if (DoCastSpellIfCan(m_creature, SPELL_TELEPORT) == CAST_OK)
+                {
+                    DoScriptText(urand(0, 1) ? SAY_EXPLOSION_1 : SAY_EXPLOSION_2, m_creature);
+                    m_creature->GetMotionMaster()->MoveIdle();
+                    m_uiTeleportTimer = urand(30000, 35000);
+                    m_uiExplosionTimer = 1000;
+                }
+            }
+            else
+                m_uiTeleportTimer -= uiDiff;
+
+            if (m_uiFrostBombTimer < uiDiff)
+            {
+                if (DoCastSpellIfCan(m_creature->getVictim(), SPELL_FROSTBOMB) == CAST_OK)
+                    m_uiFrostBombTimer = urand(5000, 8000);
+            }
+            else
+                m_uiFrostBombTimer -= uiDiff;
+
+            if (m_uiTimeBombTimer <= uiDiff)
+            {
+                if (Unit* pTarget = m_creature->SelectAttackingTarget(ATTACKING_TARGET_RANDOM, 0))
+                {
+                    if (DoCastSpellIfCan(pTarget, m_bIsRegularMode ? SPELL_TIME_BOMB : SPELL_TIME_BOMB_H) == CAST_OK)
+                        m_uiTimeBombTimer = urand(20000, 25000);
+                }
+            }
+            else
+                m_uiTimeBombTimer -= uiDiff;
+
+            DoMeleeAttackIfReady();
+        }
     }
 };
 
