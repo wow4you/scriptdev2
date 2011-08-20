3e505565af1662e32e6b68c7b87056dc70501cf7
Schmoozerd <schmoozerd@scriptdev2.com>
2011-08-20 06:42:56 +0200

    Squash - Fix Debug Info

--- scripts/eastern_kingdoms/scarlet_enclave/ebon_hold.cpp
+++ scripts/eastern_kingdoms/scarlet_enclave/ebon_hold.cpp
@@ -1518,7 +1518,7 @@ struct MANGOS_DLL_DECL npc_highlord_darion_mograineAI : public npc_escortAI
         if (m_pInstance)
             reader.PSendSysMessage("Current state for TYPE_BATTLE: %u", m_pInstance->GetData(TYPE_BATTLE));
 
-        reader.PSendSysMessage("Current Event step: %u (%s)", m_uiEventStep == 0 ? "Not-Started" : m_uiEventStep < 6 ? "Intro"  : m_uiEventStep < 9 ? "Battle" : "Outro");
+        reader.PSendSysMessage("Current Event step: %u (%s)", m_uiEventStep, m_uiEventStep == 0 ? "Not-Started" : m_uiEventStep < 7 ? "Intro"  : m_uiEventStep < 10 ? "Battle" : "Outro");
         reader.PSendSysMessage("Event-processing is %s, Fighting is %s", reader.GetOnOffStr(m_uiEventTimer), reader.GetOnOffStr(m_uiFightTimer));
     }
 
