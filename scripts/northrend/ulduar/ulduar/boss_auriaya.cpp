6d6104162216df21a1615eada5255c1e47f718c0
Schmoozerd <schmoozerd@scriptdev2.com>
2011-10-13 20:22:24 +0200

    Fix some rebase errors
    (cleaned version of 166e5cd)

--- scripts/northrend/ulduar/ulduar/boss_auriaya.cpp
+++ scripts/northrend/ulduar/ulduar/boss_auriaya.cpp
@@ -396,52 +396,40 @@ struct MANGOS_DLL_DECL mob_sanctum_sentryAI : public ScriptedAI
         DoCast(m_creature, SPELL_STRENGHT_OF_PACK);
     }
 
-    void JustDied(Unit* pKiller)
-    {
-        m_bCrazyCatLady = false;
-    }
-
     void UpdateAI(const uint32 diff)
     {
         if (!m_creature->SelectHostileTarget() || !m_creature->getVictim())
         {
             // they should follow Auriaya, but this looks ugly!
-            if (Creature* pTemp = m_pInstance->GetSingleCreatureFromStorage(NPC_AURIAYA))
-                // they should follow Auriaya, but this looks ugly!
-                if (m_creature->GetMotionMaster()->GetCurrentMovementGeneratorType() != FOLLOW_MOTION_TYPE)
+            if (m_creature->GetMotionMaster()->GetCurrentMovementGeneratorType() != FOLLOW_MOTION_TYPE)
+            {
+                Creature* pTemp = m_pInstance->GetSingleCreatureFromStorage(NPC_AURIAYA);
+                if (pTemp && pTemp->isAlive())
                 {
-                    if (pTemp->isAlive())
-                    {
-                        m_creature->GetMotionMaster()->MoveFollow(pTemp, 0.0f, 0.0f);
-                        m_creature->GetMap()->CreatureRelocation(m_creature, pTemp->GetPositionX(), pTemp->GetPositionY(), pTemp->GetPositionZ(), 0.0f);
-                    }
-                    Creature* pTemp = m_pInstance->GetSingleCreatureFromStorage(NPC_AURIAYA);
-                    if (pTemp && pTemp->isAlive())
-                    {
-                        // Calculate follow position
-                        float sX, sY, sZ, mX, mY, mZ, mO;
-                        m_creature->GetRespawnCoord(sX, sY, sZ);
-                        pTemp->GetRespawnCoord(mX, mY, mZ, &mO);
+                    // Calculate follow position
+                    float sX, sY, sZ, mX, mY, mZ, mO;
+                    m_creature->GetRespawnCoord(sX, sY, sZ);
+                    pTemp->GetRespawnCoord(mX, mY, mZ, &mO);
 
-                        float dx, dy, dz;
-                        dx = sX - mX;
-                        dy = sY - mY;
-                        dz = sZ - mZ;
+                    float dx, dy, dz;
+                    dx = sX - mX;
+                    dy = sY - mY;
+                    dz = sZ - mZ;
 
-                        float dist = sqrt(dx * dx + dy * dy + dz * dz);
-                        // TODO this code needs the same distance calculation that is used for following
-                        // Atm this means we have to subtract the bounding radiuses
-                        dist = dist - m_creature->GetObjectBoundingRadius() - pTemp->GetObjectBoundingRadius();
-                        if (dist < 0.0f)
-                            dist = 0.0f;
+                    float dist = sqrt(dx * dx + dy * dy + dz * dz);
+                    // TODO this code needs the same distance calculation that is used for following
+                    // Atm this means we have to subtract the bounding radiuses
+                    dist = dist - m_creature->GetObjectBoundingRadius() - pTemp->GetObjectBoundingRadius();
+                    if (dist < 0.0f)
+                        dist = 0.0f;
 
-                        // Need to pass the relative angle to following
-                        float angle = atan2(dy, dx) - mO;
-                        angle = (angle >= 0) ? angle : 2 * M_PI_F + angle;
+                    // Need to pass the relative angle to following
+                    float angle = atan2(dy, dx) - mO;
+                    angle = (angle >= 0) ? angle : 2 * M_PI_F + angle;
 
-                        m_creature->GetMotionMaster()->MoveFollow(pTemp, dist, angle);
-                    }
+                    m_creature->GetMotionMaster()->MoveFollow(pTemp, dist, angle);
                 }
+            }
 
             return;
         }
